51ab1209ef9acb3321e1b16bf621678d13fc6148
kimaita <johnnieqaym@gmail.com>
2022-10-13 21:52:22 +0300

    Removed fragment top app bar and bottom sheet

--- app/src/main/java/com/kimaita/monies/ui/HomeFragment.java
+++ app/src/main/java/com/kimaita/monies/ui/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.kimaita.monies.ui;
 
-import static android.view.View.INVISIBLE;
 import static com.kimaita.monies.MainActivity.loaderCallbacks;
 import static com.kimaita.monies.Utils.Constants.AIRTIME;
 import static com.kimaita.monies.Utils.Constants.CASH_DEPOSIT;
@@ -13,6 +12,8 @@ import static com.kimaita.monies.Utils.Constants.PERIOD_WEEK;
 import static com.kimaita.monies.Utils.Constants.RECEIVED;
 import static com.kimaita.monies.Utils.Constants.SENT;
 import static com.kimaita.monies.Utils.Constants.TILLS_PAYBILLS;
+import static com.kimaita.monies.Utils.MessageUtils.getMShwariBalance;
+import static com.kimaita.monies.Utils.MessageUtils.getOutstandingFuliza;
 import static com.kimaita.monies.Utils.MessageUtils.tranBalance;
 import static com.kimaita.monies.Utils.Utils.formatAmountCurrency;
 import static com.kimaita.monies.Utils.Utils.getListItems;
@@ -20,12 +21,9 @@ import static com.kimaita.monies.Utils.Utils.getListItems;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,7 +34,6 @@ import androidx.loader.app.LoaderManager;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.kimaita.monies.R;
 import com.kimaita.monies.Utils.PrefManager;
 import com.kimaita.monies.adapters.MessageAdapter;
@@ -52,11 +49,12 @@ public class HomeFragment extends Fragment {
 
     FragmentHomeBinding binding;
     public static final int LOADER = 0;
-    private static final String TAG = "Bottom Sheet Behaviour";
+
     MoneyViewModel moneyViewModel;
     HomeViewModel homeViewModel;
-    BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
+ 
     String selectedNature;
+    int selectedPeriod;
     MessageAdapter mAdapter;
 
     @NonNull
@@ -70,31 +68,14 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheetTransactions.bottomSheetTransactions);
 
-        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                bottomSheetBehavior.setPeekHeight(getBottomSheetHeight(view));
-            }
-        });
         moneyViewModel = new ViewModelProvider(this).get(MoneyViewModel.class);
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         mAdapter = new MessageAdapter(new MessageAdapter.ListItemDiff(), requireContext(), getParentFragmentManager());
-        binding.bottomSheetTransactions.recyclerHomeRecent.setAdapter(mAdapter);
+        binding.recyclerHomeRecent.setAdapter(mAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        binding.bottomSheetTransactions.recyclerHomeRecent.setLayoutManager(linearLayoutManager);
-        binding.homeToolBar.setOnMenuItemClickListener(item -> {
-            if (item.getItemId() == R.id.menu_define_ctg) {
-                Navigation.findNavController(requireView()).navigate(R.id.categoryDefineFragment);
-            }
-            if (item.getItemId() == R.id.menu_assign_ctg) {
-                Navigation.findNavController(requireView()).navigate(R.id.assignCategoriesFragment);
-            }
-            return false;
-        });
+        binding.recyclerHomeRecent.setLayoutManager(linearLayoutManager);
 
         periodSwitch(binding.homeChipGroupPeriod.getCheckedChipId());
         binding.homeChipGroupPeriod.setOnCheckedStateChangeListener((group, checkedIds) -> {
@@ -115,20 +96,36 @@ public class HomeFragment extends Fragment {
             moneyViewModel.getRecentNatureTransactions(s).observe(getViewLifecycleOwner(), messages -> {
                 List<ListItem> messageList = getListItems(messages);
                 mAdapter.submitList(messageList);
+                switch (selectedNature) {
+                    case MSHWARI:
+                        binding.mpesaDets.topSubjectLabel.setText(R.string.mshwari_bal);
+                        binding.mpesaDets.textTopSubject.setText(formatAmountCurrency(getMShwariBalance(messages.get(0).msg)));
+                        break;
+                    case FULIZA:
+                        binding.mpesaDets.topSubjectLabel.setText(R.string.outstanding_fuliza);
+                        binding.mpesaDets.textTopSubject.setText(formatAmountCurrency(getOutstandingFuliza(messages.get(0).msg)));
+                        break;
+                    default:
+                        binding.mpesaDets.topSubjectLabel.setText(R.string.top_subject);
+                }
             });
-            binding.bottomSheetTransactions.transactionsLabel.setText(getString(R.string.recent_type_transactions, s));
+            binding.transactionsLabel.setText(getString(R.string.recent_type_transactions, s));
 
             homeViewModel.getSelectedPeriod().observe(getViewLifecycleOwner(), integer -> {
+                selectedPeriod = integer;
                 moneyViewModel.getTransactionCount(s, integer).observe(getViewLifecycleOwner(), aDouble ->
                         binding.mpesaDets.textTransactionCount.setText(String.valueOf(aDouble)));
                 moneyViewModel.getTransactionVolume(s, integer).observe(getViewLifecycleOwner(), aDouble -> {
-                    if (aDouble != null)
+                    if ((aDouble != null)) {
                         binding.mpesaDets.textTransactionVol.setText(formatAmountCurrency(aDouble));
-                    else binding.mpesaDets.textTransactionVol.setText("--");
+                    } else {
+                        binding.mpesaDets.textTransactionVol.setText("--");
+                    }
                 });
-                moneyViewModel.getTopSubject(s, integer).observe(getViewLifecycleOwner(), s1 ->
-                        binding.mpesaDets.textTopSubject.setText(s1));
-
+                moneyViewModel.getTopSubject(s, integer).observe(getViewLifecycleOwner(), s1 -> {
+                    if (!s.equals(MSHWARI) || !s.equals(FULIZA))
+                        binding.mpesaDets.textTopSubject.setText(s1);
+                });
                 moneyViewModel.getGrowth(s, integer).observe(getViewLifecycleOwner(),
                         aDouble -> moneyViewModel.getGrowthPercent(s, integer).observe(getViewLifecycleOwner(),
                                 aDouble1 -> setGrowthText(aDouble, aDouble1)));
@@ -150,6 +147,7 @@ public class HomeFragment extends Fragment {
                         binding.mpesaDets.textTopSubject.setText(s1));
             });
         }
+
         binding.mpesaCards.cardAirtime.setOnClickListener(view1 -> homeViewModel.setNature(AIRTIME));
         binding.mpesaCards.cardDeposits.setOnClickListener(view12 -> homeViewModel.setNature(CASH_DEPOSIT));
         binding.mpesaCards.cardFuliza.setOnClickListener(view13 -> homeViewModel.setNature(FULIZA));
@@ -159,7 +157,8 @@ public class HomeFragment extends Fragment {
         binding.mpesaCards.cardWithdrawal.setOnClickListener(view17 -> homeViewModel.setNature(CASH_WITHDRAWAL));
         binding.mpesaCards.cardSent.setOnClickListener(view18 -> homeViewModel.setNature(SENT));
 
-        bottomSheetBehavior.addBottomSheetCallback(createBottomSheetBehavior());
+        binding.transactionsLabel.setOnClickListener(view19 ->
+                Navigation.findNavController(view19).navigate(HomeFragmentDirections.actionNavigationHomeToTransactionsFragment()));
 
         binding.swipeRefresh.setOnRefreshListener(() -> {
             binding.swipeRefresh.setRefreshing(false);
@@ -204,61 +203,8 @@ public class HomeFragment extends Fragment {
                 break;
         }
     }
-
-    private BottomSheetBehavior.BottomSheetCallback createBottomSheetBehavior() {
-        return new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        if (requireActivity().findViewById(R.id.bottom_nav).getVisibility() == View.INVISIBLE)
-                            requireActivity().findViewById(R.id.bottom_nav).setVisibility(View.VISIBLE);
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        requireActivity().findViewById(R.id.bottom_nav).setVisibility(INVISIBLE);
-
-                        if (selectedNature != null)
-                            moneyViewModel.getAllNatureTransactions(selectedNature).observe(getViewLifecycleOwner(),
-                                    messages -> {
-                                        List<ListItem> messageList = getListItems(messages);
-                                        mAdapter.submitList(messageList);
-                                    });
-                        else
-                            moneyViewModel.getRecentTransactions().observe(getViewLifecycleOwner(), messages -> {
-                                List<ListItem> messageList = getListItems(messages);
-                                mAdapter.submitList(messageList);
-                            });
-
-                        break;
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        Log.d(TAG, "STATE_HIDDEN");
-                        break;
-                }
-            }
-
-            @Override
-            public void onSlide(View bottomSheet, float slideOffset) {
-
-            }
-        };
-    }
-
-    int getBottomSheetHeight(View v) {
-        return getResources().getDisplayMetrics().heightPixels - getOccupiedHeight(v) - 64;
-    }
-
-    private int getOccupiedHeight(View view) {
-        return view.findViewById(R.id.text_current_bal).getMeasuredHeight() +
-                view.findViewById(R.id.text_bal_label).getMeasuredHeight() +
-                view.findViewById(R.id.home_app_bar).getMeasuredHeight() +
-                view.findViewById(R.id.mpesa_cards).getMeasuredHeight() +
-                view.findViewById(R.id.mpesa_dets).getMeasuredHeight() +
-                view.findViewById(R.id.home_layoutChips).getMeasuredHeight();
-
-    }
-
-}
+    
+  }
 
 
 
